2d6e0a35b32746cc95748756533929ecc71801c7
Iuliia Sadikova <prettysquirrel@gmail.com>
2023-01-31 20:50:26 +0100

    Change text to "Hello world"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String text = "Our new text";
+        String text = "Hello world!";
         System.out.println(text);
     }
 }
